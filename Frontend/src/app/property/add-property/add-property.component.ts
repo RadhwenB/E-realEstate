b32c34af595deb8153669340e828b35671139e72
RadhwenB <bouaounradhwen@gmail.com>
2020-10-25 10:43:27 +0100

    adding routes

---
+++ Frontend/src/app/property/add-property/add-property.component.ts
@@ -0,0 +1,21 @@
+import { Route } from '@angular/compiler/src/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-add-property',
+  templateUrl: './add-property.component.html',
+  styleUrls: ['./add-property.component.css']
+})
+export class AddPropertyComponent implements OnInit {
+
+  constructor(private router : Router) { }
+
+  ngOnInit(): void {
+  }
+
+  onBack(){
+    this.router.navigate(['/']);
+  }
+
+}
