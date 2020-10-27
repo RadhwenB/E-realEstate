d9e9e75dd582a2f42052ff30b3ac45184064f9ec
RadhwenB <bouaounradhwen@gmail.com>
2020-10-27 17:29:59 +0100

    Add user authentification

--- Frontend/src/app/user/user-login/user-login.component.ts
+++ Frontend/src/app/user/user-login/user-login.component.ts
@@ -1,4 +1,9 @@
+import { NgForOf } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AlertifyService } from 'src/app/services/alertify.service';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-user-login',
@@ -7,9 +12,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserLoginComponent implements OnInit {
 
-  constructor() { }
+  constructor(private authService: AuthService, private alertify: AlertifyService, private router: Router) { }
 
   ngOnInit(): void {
+  }
+
+  onLogin(loginForm: NgForm){
+    const token = this.authService.authUser(loginForm.value);
+    if(token){
+      localStorage.setItem('token', token.userName);
+      this.alertify.success('Login Successful');
+      this.router.navigate(['/']);
+    } else {
+      this.alertify.error('User name or password is wrong');
+    }
   }
 
 }
