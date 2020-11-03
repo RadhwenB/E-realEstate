2093347c6c2b773834a6a75cc4c1baf39c1b9da0
RadhwenB <bouaounradhwen@gmail.com>
2020-11-03 18:13:49 +0100

    save and display properties from local storage

--- Frontend/src/app/property/property-list/property-list.component.ts
+++ Frontend/src/app/property/property-list/property-list.component.ts
@@ -23,12 +23,6 @@ export class PropertyListComponent implements OnInit {
     this.housingService.getAllProperties(this.SellRent).subscribe(
       data=>{
         this.properties=data;
-        const newProperty = JSON.parse(localStorage.getItem('newProp'));
-
-        if(newProperty.SellRent == this.SellRent){
-          this.properties = [newProperty, ...this.properties];
-        }
-
       }, error => {
         console.log(error);
       }
--- Frontend/src/app/services/housing.service.ts
+++ Frontend/src/app/services/housing.service.ts
@@ -17,6 +17,14 @@ export class HousingService {
     return this.http.get('data/properties.json').pipe(
       map(data => {
         const propertiesArray: Array<IPropertyBase> = [];
+        const localProperties = JSON.parse(localStorage.getItem('newProp'));
+        if(localProperties){
+          for(const id in localProperties){
+            if(localProperties.hasOwnProperty(id) && localProperties[id].SellRent === SellRent){
+              propertiesArray.push(localProperties[id]);
+            }
+          }
+        }
         for(const id in data){
           if(data.hasOwnProperty(id) && data[id].SellRent === SellRent){
             propertiesArray.push(data[id]);
@@ -27,6 +35,21 @@ export class HousingService {
     )
   }
   addProperty(property: Property){
-    localStorage.setItem('newProp', JSON.stringify(property));
+    let newProp = [property];
+    if(localStorage.getItem('newProp')){
+      newProp = [property, ...JSON.parse(localStorage.getItem('newProp'))];
+    }
+    localStorage.setItem('newProp', JSON.stringify(newProp));
   }
+
+  newPropId(){
+    if(localStorage.getItem('PID')){
+      localStorage.setItem('PID', String(+localStorage.getItem('PID') + 1 ));
+      return +localStorage.getItem('PID');
+    }else{
+      localStorage.setItem('PID', '101');
+      return 101;
+    }
+  }
+
 }
